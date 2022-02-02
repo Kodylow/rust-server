2afc0137003c9e139048342fff079e21ad32b0d2
Kody Low <kodylow7@gmail.com>
2022-02-01 21:48:08 -0500

    rust basic server setup

---
+++ src/http/request.rs
@@ -0,0 +1,36 @@
+use super::method::Method;
+use std::convert::TryFrom;
+
+pub struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {}
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
+// trait Encrypt {
+//     fnencrypt(&self) -> Self;
+// }
+
+// impl Encrypt for String {
+//     fn encrypt(&self) -> Self {
+//         unimplemented!()
+//     }
+// }
+
+// impl Encrypt for &[u8] {
+//     fn encrypt(&self) -> Self {
+//         unimplemented!()
+//     }
+// }
---
+++ src/server.rs
@@ -0,0 +1,45 @@
+use std::net::TcpListener;
+use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    //Constructor method for struct always called new 
+    //All structs have embedded Self type, same as "Server" here
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    //don't want to take ownership so make it a reference
+    pub fn run(&self) {
+        println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+          match listener.accept() {
+            Ok((mut stream, _)) => {
+              let mut buffer = [0; 1024];
+              match stream.read(&mut buffer) {
+                Ok(_) => {
+                  println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                  match Request::try_from(&buffer[..]) {
+                    Ok(request) => {},
+                    Err(e) => println!("Failed to parse a request: {}", e)
+                  }
+                  let res: &Result<Request, _> = &buffer[..].try_into();
+                },
+                Err(e) => println!("Failed to read from connection: {}", e),
+              }
+            }
+            Err(e) => println!("Failed to establish a connection: {}", e),
+          }
+        }
+    }
+}
